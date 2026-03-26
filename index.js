107f9963fe984ccdf319aec793b738587eaffcf3
Monica <tudormonica1996@yahoo.com>
2026-03-26 20:10:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const path= require("path");
+
+app= express();
+app.set("view engine", "ejs")
+
+console.log("Folder index.js", __dirname);
+console.log("Folder curent (de lucru)", process.cwd());
+console.log("Cale fisier", __filename);
+
+
+
+
+
+app.listen(8080);
+console.log("Serverul a pornit!");
